db3c755ff9e5872e77782a5158673826a25a65cd
khainqd <khainqd00575@fpt.edu.vn>
2018-11-10 09:35:13 +0700

    Entity Framework Database

---
+++ EntityFrameworkDatabaseFirst/EntityFrameworkDatabaseFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntityFrameworkDatabaseFirst.Startup))]
+namespace EntityFrameworkDatabaseFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
